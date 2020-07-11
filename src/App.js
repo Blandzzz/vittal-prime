c235215683f5dcf64260919fe9564f8349a1211e
victor.castro <vcastro2395@gmail.com>
2020-07-11 20:58:32 -0300

    Fixed pages importing

--- src/App.js
+++ src/App.js
@@ -1,41 +1,31 @@
-import React, { Component } from 'react';
-import Users from './containers/Users/Users';
+import React from 'react';
+import Users from './pages/users/Users';
 import NewUser from './pages/newUser/NewUser';
 import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
 import './App.css';
 
 
-class App extends Component {
-  state = {
-    users: []
-  }
-
-  render() {
-    
-    return (
+function App (props) {
+  return (
       <div className="App">
-        <div className="App-nav">
-          <nav>
-            <ul>
-              <li><a href="/schedule"><i class="fas fa-calendar-alt"></i></a></li>
-              <li><a href="/users"><i className="fa fa-users"></i></a></li>
-              <li><a href="/options"><i className="fas fa-cog"></i></a></li>
-            </ul>
-          </nav>
-        </div>
-
-        <div className="App-body">
-          <Router>
-            <Route path='/' exact>
-              <Redirect to="/schedule"></Redirect>
-            </Route>
-            <Route path='/users' exact render={() => <Users></Users>}></Route>
-            <Route path='/users/new' exact render={() => <NewUser></NewUser>}></Route>
-          </Router>
-        </div>
+        <Router>
+          <div className="App-nav">
+            <nav>
+              <ul>
+                <li><a href="/schedule"><i className="fas fa-calendar-alt"></i></a></li>
+                <li><a href="/users"><i className="fa fa-users"></i></a></li>
+                <li><a href="/options"><i className="fas fa-cog"></i></a></li>
+              </ul>
+            </nav>
+          </div>
+          <Route path='/' exact>
+            <Redirect to="/schedule"></Redirect>
+          </Route>
+          <Route path='/users' exact render={() => <Users></Users>}></Route>
+          <Route path='/users/new' exact render={() => <NewUser></NewUser>}></Route>
+        </Router>
       </div>
-    );
-  }
+  );
 }
 
 export default App;
