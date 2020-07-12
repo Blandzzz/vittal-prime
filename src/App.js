0562cf6eefc68b3900a3a77770a82624d888f73b
victor.castro <vcastro2395@gmail.com>
2020-07-11 22:26:57 -0300

    Added header, changed favicon

--- src/App.js
+++ src/App.js
@@ -7,24 +7,41 @@ import './App.css';
 
 function App (props) {
   return (
-      <div className="App">
-        <Router>
-          <div className="App-nav">
-            <nav>
-              <ul>
-                <li><a href="/schedule"><i className="fas fa-calendar-alt"></i></a></li>
-                <li><a href="/users"><i className="fa fa-users"></i></a></li>
-                <li><a href="/options"><i className="fas fa-cog"></i></a></li>
-              </ul>
-            </nav>
-          </div>
-          <Route path='/' exact>
-            <Redirect to="/schedule"></Redirect>
-          </Route>
-          <Route path='/users' exact render={() => <Users></Users>}></Route>
-          <Route path='/users/new' exact render={() => <NewUser></NewUser>}></Route>
-        </Router>
+    <div className="App">
+      <div className="App-header">
+        <ul className="App-user-panel">
+          <li>
+            <a href="/#">
+              <i className="fas fa-bell"></i>
+            </a>
+          </li>
+          <li>
+            <a href="/#">
+              <span className="App-user-icon">U</span>
+            </a>
+          </li>
+        </ul>
       </div>
+
+      <div className="App-container">
+        <div className="App-nav">
+          <ul>
+            <li><a href="/schedule"><i className="far fa-calendar-alt"></i></a></li>
+            <li><a href="/users"><i className="fa fa-users"></i></a></li>
+            <li><a href="/options"><i className="fas fa-cog"></i></a></li>
+          </ul>
+        </div>
+        <div className="App-pages">
+          <Router>
+            <Route path='/' exact>
+              <Redirect to="/schedule"></Redirect>
+            </Route>
+            <Route path='/users' exact render={() => <Users></Users>}></Route>
+            <Route path='/users/new' exact render={() => <NewUser></NewUser>}></Route>
+          </Router>
+        </div>
+      </div>
+    </div>  
   );
 }
 
